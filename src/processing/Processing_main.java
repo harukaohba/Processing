739f3564a4a08d258a30fbe77b41ac8972dcb286
haruka <haruka@192.168.8.101>
2018-09-12 23:08:42 +0900

    processing Javaでできるようにする

---
+++ src/processing/Processing_main.java
@@ -0,0 +1,35 @@
+package processing;
+
+import processing.core.PApplet;
+
+public class Processing_main extends PApplet{
+
+	int x, y;
+	 
+    public void settings(){
+        size( 600, 600 );     
+    }
+ 
+    public void setup() {
+        background( 50 );
+    }
+ 
+    public void mouseMoved(){
+        x = mouseX;
+        y = mouseY;
+    }
+ 
+    public void draw() {
+        ellipse(x, y, 50, 50);
+        
+    }
+    
+    public static void main(String[] args) {
+    		//PApplet.main( "Processing_main" );
+    	
+    		String[] a = {"MAIN"};
+        PApplet.runSketch( a, new Processing_main());
+    }
+
+
+}
