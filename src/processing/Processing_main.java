9e4b043ff276318098436f55c5176298178eebd0
haruka <haruka@172.16.219.1>
2018-09-18 18:09:20 +0900

    fishmoveを追加。色・線の太さでエラーが出る

--- src/processing/Processing_main.java
+++ src/processing/Processing_main.java
@@ -2,34 +2,33 @@ package processing;
 
 import processing.core.PApplet;
 
-public class Processing_main extends PApplet{
+public class Processing_main extends PApplet {
 
 	int x, y;
-	 
-    public void settings(){
-        size( 600, 600 );     
-    }
- 
-    public void setup() {
-        background( 50 );
-    }
- 
-    public void mouseMoved(){
-        x = mouseX;
-        y = mouseY;
-    }
- 
-    public void draw() {
-        ellipse(x, y, 50, 50);
-        
-    }
-    
-    public static void main(String[] args) {
-    		//PApplet.main( "Processing_main" );
-    	
-    		String[] a = {"MAIN"};
-        PApplet.runSketch( a, new Processing_main());
-    }
 
+	public void settings() {
+		size(600, 600);
+	}
+
+	public void setup() {
+		background(50);
+	}
+
+	public void mouseMoved() {
+		x = mouseX;
+		y = mouseY;
+	}
+
+	public void draw() {
+		ellipse(x, y, 50, 50);
+
+	}
+
+	public static void main(String[] args) {
+		// PApplet.main( "Processing" );
+
+		String[] a = { "MA" };
+		PApplet.runSketch(a, new Processing_main());
+	}
 
 }
